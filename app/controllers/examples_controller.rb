2dff0aefb38b821213f712a561a3ffa67c82ebd5
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2020-06-10 21:35:19 +0300

    Add form to view

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -1,15 +1,17 @@
-class HomeController < ApplicationController
+class ExamplesController < ApplicationController
   def index
-    sum_range = params[:sum_range] || (0..10).to_a
-    difference_range = params[:sum_range] || (1..10).to_a
-    multiplication_range = params[:multiplication_range] || (0..10).to_a
-    devider_range = params[:multiplication_range] || (1..10).to_a
+    render :index, locals: { examples: [] } and return unless params[:generate]
+
+    sum_range = (0..params[:sum_range].to_i).to_a
+    difference_range = (0..params[:difference_range].to_i).to_a
+    multiplication_range = (0..params[:multiplication_range].to_i).to_a
+    divider_range = (1..params[:divider_range].to_i).to_a
 
     difference_collection =
       difference_range.each_with_object({}) { |x, memo| memo.merge!(x => difference_range.select { |y| x <= y }) }
 
     devider_collection =
-      devider_range.each_with_object({}) { |x, memo| memo.merge!(x => devider_range.select { |y| x%y == 0 }) }
+      divider_range.each_with_object({}) { |x, memo| memo.merge!(x => divider_range.select { |y| x%y == 0 }) }
 
     potential_cells = {
       multiplication: [],
@@ -49,7 +51,7 @@ class HomeController < ApplicationController
 
     # mask = params[:mask]
 
-    examples = potential_cells.map { |_, values| values.map { |x| x + "=" } }.flatten
+    examples = potential_cells.map { |_, values| values.map { |x| x + "=" } }.flatten.shuffle.first(160)
     render :index, locals: { examples: examples }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root to: "home#index"
+  root to: "examples#index"
 end
