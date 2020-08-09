359761ede437a4279827f7d15182342e35684c7d
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2020-08-09 21:46:33 +0300

    Fix incorrect results

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -4,16 +4,16 @@ class ExamplesController < ApplicationController
 
     render :index, locals: { examples: [], masks: masks } and return unless params[:generate]
 
-    @sum_range = (0..params[:sum_range].to_i).to_a
-    @difference_range = (0..params[:difference_range].to_i).to_a
-    @multiplication_range = (0..params[:multiplication_range].to_i).to_a
-    @divider_range = (1..params[:divider_range].to_i).to_a
+    %i(divider_range multiplication_range difference_range sum_range).each do |range|
+      value = params[range].blank? ? [] : (0..params[range].to_i).to_a
+      instance_variable_set("@#{range}", value)
+    end
 
     difference_collection =
       @difference_range.each_with_object({}) { |x, memo| memo.merge!(x => @difference_range.select { |y| x <= y }) }
 
     devider_collection =
-      @divider_range.each_with_object({}) { |x, memo| memo.merge!(x => @divider_range.select { |y| x%y == 0 }) }
+      @divider_range.each_with_object({}) { |x, memo| memo.merge!(x => @divider_range[1..-1].select { |y| x%y == 0 }) }
 
     potential_cells = {
       multiplication: [],
@@ -25,7 +25,7 @@ class ExamplesController < ApplicationController
     potential_cells[:multiplication] = @multiplication_range.map { |x| @multiplication_range.map { |y| ["#{x}*#{y}", "#{y}*#{x}"] } }.flatten.uniq
     potential_cells[:sum] = @sum_range.map { |x| @sum_range.map { |y| ["#{x}+#{y}", "#{y}+#{x}"]} }.flatten.uniq
     potential_cells[:difference] = difference_collection.map { |key, value| value.map { |x| "#{key}-#{x}"} }.flatten
-    potential_cells[:devider] = devider_collection.map { |key, value| value.map { |x| "#{key}/#{x}"} }.flatten
+    potential_cells[:devider] = devider_collection.map { |key, value| value.map { |x| "#{key}/#{x.to_f}"} }.flatten
 
     require_masks_ids = params[:masks].flatten.map(&:to_i).reject { |mask| mask == 0 }
 
@@ -47,6 +47,7 @@ class ExamplesController < ApplicationController
     sums = examples.flatten.select { |x| eval(x) < params[:max_result].to_i }.map { |x| x + "=" }.sample(150)
     sums.map { |x| x.gsub!("*", "x") }
     sums.map { |x| x.gsub!("/", ":") }
+    sums.map { |x| x.gsub(".0", "") }
 
     render :index, locals: { examples: sums, masks: masks }
   end
@@ -65,24 +66,36 @@ class ExamplesController < ApplicationController
   end
 
   def add_sign_to_sums(sign, sums, digital_series)
-    correct_sums = sums.flatten.select { |x| eval(x).in?(digital_series) }
-    result = digital_series.map do |digital|
+    correct_sums =
+      if sign == "/" || sign = "*"
+        sums.flatten.select { |x| eval(x).in?(digital_series) }
+      else
+        sums.flatten.select { |x| eval(x) <= params[:max_result] }
+      end
+
+    results = digital_series.map do |digital|
       correct_sums.map do |sum|
-        sum + sign + digital.to_s
+        if sign == "/"
+          sum + sign + digital.to_s if digital != 0 && eval(sum)%digital == 0
+        else
+          sum + sign + digital.to_s
+        end
       end
     end.flatten
     condition_digital = sign == "/" ? 1 : 0
 
-    math_result = []
+    math_results = []
 
-    while math_result.size < 150
-      return math_result if result.empty?
+    while math_results.size < 150
+      return math_results if results.empty?
 
-      new_result = result.shuffle.shift(150)
-      math_result += new_result.select { |x| eval(x) >= condition_digital }
+      new_results = results.shuffle.shift(150)
+      math_results += new_results.compact.select do |x|
+        eval(x) >= condition_digital && eval(x) <= params[:max_result].to_i
+      end
     end
 
-    math_result
+    math_results
   end
 
   def potential_cells_results(collection)
